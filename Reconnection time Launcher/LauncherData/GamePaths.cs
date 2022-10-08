3f790f4df01762f138753f46cbfb5e05b386d603
iojuedioe9rd <catsstudio81@gmail.com>
2022-10-08 16:36:42 +0100

    Add project files.

---
+++ Reconnection time Launcher/LauncherData/GamePaths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reconnection_time_Launcher.LauncherData
+{
+    public class GamePaths
+    {
+        public string ExeFile = string.Empty;
+        public string GamesDir = string.Empty;
+        public string GameVerFile = string.Empty;
+
+        public GamePaths(string Version)
+        {
+            GamesDir = Path.Combine(Environment.CurrentDirectory, "Vesions");
+            GameVerFile = Path.Combine(GamesDir, $"ver{Version}");
+            ExeFile = Path.Combine(GameVerFile, $"Build({Version})", "Reconnection time.exe");
+        }
+    }
+}
---
+++ Reconnection time Launcher/LauncherData/VersionManger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reconnection_time_Launcher.LauncherData
+{
+    public class VersionManger
+    {
+        public Dictionary<string, string> Ver;
+
+        private MainWindow mainWindow;
+        public VersionManger(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+    }
+}
